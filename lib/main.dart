731a61b1e6b82817ec63023dff3fb27159c63c3f
Aurelia <aurelaurelzr@gmail.com>
2026-01-16 19:07:07 +0700

    gw ngulang co

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wondertall/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen()
+      home: const Placeholder()
     );
   }
 }
